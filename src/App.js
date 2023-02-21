07b70135f11724367d70ae925fbba3bdf468df16
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-02-21 17:16:12 +0530

    App initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css'; 
-
+import {useState, useEffect} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 function App() {
+  const [quote, setQuote] = useState('');
+
+  let getQuote = async () =>{
+    let response = await fetch('https://api.adviceslip.com/advice').then((response=> response.json()));
+    setQuote(response.slip.advice);
+    console.log("Click button");
+  }
+
+  useEffect(() => {getQuote()}, []);
+
   return (
-    <h1>Quote</h1>
+      <div className='card'>
+        <h1 className='quote'>{quote}</h1>
+        <div className='buttons'>
+          <button className='getQuote' onClick={getQuote}>
+            <span>Get Quote</span>
+          </button>
+          <button className='tweet' href>
+            <a href = {`https://twitter.com/intent/tweet?text=${quote}`}>
+              <FontAwesomeIcon icon={faTwitter} className='icon'/> 
+              <span>Tweet</span>
+            </a>
+          </button>
+        </div>
+      </div>
   );
 }
 
